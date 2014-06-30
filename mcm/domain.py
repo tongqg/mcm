9a9f9aefbbd0b4cf2d4c28f99082fdcccf14c032
Jonathan Tong <tongqg@cn.ibm.com>
2014-06-30 13:30:29 +0800

    finished domain xml

--- mcm/domain.py
+++ mcm/domain.py
@@ -4,12 +4,14 @@ __author__='tongqg'
 import logging
 import uuid
 import unittest
+import random
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 from util import xmltool
 
 log = logging.getLogger('domain')
 
-def gen(name, vcpu, mem, memunit):
+def gen(name, vcpu, mem, memunit, vda, cdrom=None, logfile='mcm.log'):
 	jroot = {}
 	jd = {}
 	jd['__type'] = 'kvm'
@@ -36,16 +38,48 @@ def gen(name, vcpu, mem, memunit):
 	jsysinfo['system'] = {'entry': jent}
 	jd['sysinfo'] = jsysinfo	
 
+	jd['os'] = {'type':{'__arch':'x86_64', '__machine':'rhel6.5.0', '__text':'hvm'}, 
+				'boot':{'__dev':'hd'}, 'smbios':{'__mode':'sysinfo'}}
+	jdevices = {'emulator':'/usr/libexec/qemu-kvm'}
+	jharddisk = {'__type' : 'file', '__device':'disk', 
+				'driver':{'__name':'qemu', '__type':'qcow2', '__cache':'none'},
+				'source':{'__file':vda},
+				'target':{'__dev':'vda', '__bus':'virtio'}}
+	if cdrom is not None:
+		jcdrom = {'__type' : 'file', '__device':'cdrom', 
+				'driver':{'__name':'qemu', '__type':'raw'},
+				'source':{'__file':cdrom},
+				'target':{'__dev':'hda', '__bus':'ide'}}
+		jdevices['disk'] = [jharddisk, jcdrom]
+	else:
+		jdevices['disk'] = jharddisk
+	jd['devices'] = jdevices
+	jd['interface'] = {'__type':'bridge', 'mac':{'__address':randomMAC()}, 'source':{'__bridge':'br0'},
+						'model':{'__type':'virtio'}, 'fileterref':{'__fileter':'clean-traffic'}}
+	jd['console'] = {'__type': 'file', 'source':{'__path':logfile}, 'target':{'__type':'serial', '__port':'0'}}
 	jroot['domain']=jd
 
 	return jroot
 
+def randomMAC():
+	mac = [ 0x00, 0x16, 0x3e,
+		random.randint(0x00, 0x7f),
+		random.randint(0x00, 0xff),
+		random.randint(0x00, 0xff) ]
+	return ':'.join(map(lambda x: "%02x" % x, mac))
 
 class UnitTest(unittest.TestCase):
 	logging.basicConfig(level=logging.DEBUG)
 	def test_gen(self):
-		print ET.dump(xmltool.jsonToXML(gen('a', 2, 2, 'KiB')))
+		self.prettyprint(xmltool.jsonToXML(gen('a', 2, 2, 'KiB', 'filepath')))
+		self.prettyprint(xmltool.jsonToXML(gen('a', 2, 2, 'KiB', 'filepath','isopath')))
 		return
+
+	def prettyprint(self, root):
+		domstr = ET.tostring(root)
+		newdom = minidom.parseString(domstr)
+		print newdom.toprettyxml(indent='  ')
+
 
 if __name__ == "__main__":
 	unittest.main()
